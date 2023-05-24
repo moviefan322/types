8f1514c6cc9e5a0ba9146ab69fcd8d32ed31ba49
moviefan322 <philman202@yahoo.com>
2023-05-23 21:30:54 -0400

    assigns types to an array

--- app.ts
+++ app.ts
@@ -5,6 +5,15 @@
 const person = {
   name: "Todd",
   age: 27,
+  hobbies: ["Sports", "Cooking"],
 };
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  //   console.log(hobby.map()); // !!! ERROR !!!
+}
 
 console.log(person);
