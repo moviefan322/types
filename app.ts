473a20736e794ac848b02f4961abfbfb08d300f1
moviefan322 <philman202@yahoo.com>
2023-05-23 20:37:41 -0400

    assinging types with boolean and string

--- app.ts
+++ app.ts
@@ -1,10 +1,20 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
+function add(
+  n1: number,
+  n2: number,
+  showResult: boolean,
+  phrase: string
+): number | void {
+  const result: number = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 }
 
 const number1: number = 5;
 const number2: number = 2.8;
+const printResult: boolean = true;
+const resultPhrase = "Result is: ";
 
-const result = add(number1, number2);
-
-console.log(result);
+const result = add(number1, number2, printResult, resultPhrase);
