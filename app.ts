d6626c53fe31b7117c4caa78ce7651eb203c9feb
moviefan322 <philman202@yahoo.com>
2023-05-23 21:22:10 -0400

    assigns types to an object

--- app.ts
+++ app.ts
@@ -1,20 +1,10 @@
-function add(
-  n1: number,
-  n2: number,
-  showResult: boolean,
-  phrase: string
-): number | void {
-  const result: number = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+const person = {
+  name: "Todd",
+  age: 27,
+};
 
-const number1: number = 5;
-const number2: number = 2.8;
-const printResult: boolean = true;
-const resultPhrase = "Result is: ";
-
-const result = add(number1, number2, printResult, resultPhrase);
+console.log(person);
---
+++ basics.ts
@@ -0,0 +1,20 @@
+function add(
+  n1: number,
+  n2: number,
+  showResult: boolean,
+  phrase: string
+): number | void {
+  const result: number = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+const result = add(number1, number2, printResult, resultPhrase);
