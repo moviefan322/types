91e9b2d4183b28e2d91fae3ce70ea0cbe013866f
moviefan322 <philman202@yahoo.com>
2023-05-31 14:59:17 -0400

    creates discriminated unions with horse and bird

--- dist/src/app.js
+++ dist/src/app.js
@@ -43,3 +43,15 @@ function useVehicle(vehicle) {
 }
 useVehicle(v1);
 useVehicle(v2);
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+    }
+    console.log("Moving at speed: " + speed);
+}
+moveAnimal({ type: "bird", flyingSpeed: 10 });
