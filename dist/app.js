8ca1cb8662af66dd77d090773d01106f0df3054f
moviefan322 <philman202@yahoo.com>
2023-05-31 18:17:27 -0400

    working with generic classes

--- dist/app.js
+++ dist/app.js
@@ -13,3 +13,48 @@ function merge(objA, objB) {
 }
 const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 console.log(mergedObj.name);
+function countAndDescribe(element) {
+    let descriptionText = "Got no value.";
+    if (element.length === 1) {
+        descriptionText = "Got 1 element.";
+    }
+    else if (element.length > 1) {
+        descriptionText = "Got " + element.length + " elements.";
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe("Hi there!"));
+function extractAndConvert(obj, key) {
+    return "Value" + obj[key];
+}
+extractAndConvert({ name: "Max" }, "name");
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1); // -1
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Max");
+console.log(textStorage.getItems());
+const numberStorage = new DataStorage();
+// const objStorage = new DataStorage<object>();
+// const maxObj = { name: "Max" };
+// objStorage.addItem(maxObj);
+// objStorage.addItem({ name: "Manu" });
+// // ...
+// objStorage.removeItem(maxObj);
+// console.log(objStorage.getItems());
