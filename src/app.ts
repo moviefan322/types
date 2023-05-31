26ef0ca72341e16a8671ec0bb6222ff536302dfb
moviefan322 <philman202@yahoo.com>
2023-05-31 12:37:22 -0400

    adds optional properties to classes

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,11 @@ let add: AddFn;
 
 add = (n1: number, n2: number) => {
   return n1 + n2;
-}
+};
 
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -18,21 +19,27 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
-    console.log(phrase + " " + this.name);
+    if (this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log("Hi!");
+    }
   }
 }
 
 let user1: Greetable;
 
-user1 = new Person("Max");
+user1 = new Person();
 
 user1.greet("Hi there - I am");
 console.log(user1);
