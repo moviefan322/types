1874553a52e3ca6b98509637d4649110ba45f2a8
moviefan322 <philman202@yahoo.com>
2023-05-31 15:29:02 -0400

    illustrates nullish coalescer

--- dist/app.js
+++ dist/app.js
@@ -19,6 +19,9 @@ const fetchedUserData = {
     job: { title: "CEO", description: "My own company" },
 };
 console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
+const userInput = null;
+const storedData = userInput !== null && userInput !== void 0 ? userInput : "DEFAULT";
+console.log(storedData);
 // type UnknownEmployee = Employee | Admin;
 // function printEmployeeInformation(emp: UnknownEmployee) {
 //   console.log("Name: " + emp.name);
--- src/app.ts
+++ src/app.ts
@@ -46,6 +46,12 @@ const fetchedUserData = {
 
 console.log(fetchedUserData?.job?.title);
 
+const userInput = null;
+
+const storedData = userInput ?? "DEFAULT";
+
+console.log(storedData);
+
 // type UnknownEmployee = Employee | Admin;
 
 // function printEmployeeInformation(emp: UnknownEmployee) {
