94bc5b37833193733a64f6f1c86addf75ac2b348
moviefan322 <philman202@yahoo.com>
2023-05-31 20:45:23 -0400

    adds an autobind decorator

--- dist/app.js
+++ dist/app.js
@@ -94,3 +94,29 @@ __decorate([
 ], Product.prototype, "getPriceWithTax", null);
 new Product("Book", 19);
 new Product("Book 2", 29);
+function Autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjDescriptor;
+}
+class Printer {
+    constructor() {
+        this.message = "This works!";
+    }
+    showMessage() {
+        console.log(this.message);
+    }
+}
+__decorate([
+    Autobind
+], Printer.prototype, "showMessage", null);
+const p = new Printer();
+const button = document.querySelector("button");
+button.addEventListener("click", p.showMessage);
--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,10 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookdId: string) {
-    console.log("TEMPLATE FACTORY")
-  return function<T extends {new(...args: any[]): {name: string}}>(originalConstructor: T) {
+  console.log("TEMPLATE FACTORY");
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
     return class extends originalConstructor {
       constructor(..._: any[]) {
         super();
@@ -95,3 +97,32 @@ class Product {
 
 new Product("Book", 19);
 new Product("Book 2", 29);
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+
+  return adjDescriptor;
+}
+
+class Printer {
+  message = "This works!";
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
+ 
